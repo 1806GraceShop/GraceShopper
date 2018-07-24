8aa1ea16e509d66c590630317331bf0c007c924d
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-07-24 10:21:12 -0500

    singularized the route name

--- server/api/products.js
+++ server/api/products.js
@@ -1,12 +1,10 @@
 const router = require('express').Router()
-const {Products} = require('../db/models')
+const {Product} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const products = await Products.findAll({
-        attributes: ['title', 'description', 'price', 'quantity']
-    })
+    const products = await Products.findAll({})
     res.json(products)
   } catch (err) {
     next(err)
