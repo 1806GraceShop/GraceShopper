be5c5f38c3b2626a08805242805091085888bbd8
Diego Hernandorena <dhernandorena@gmail.com>
2018-07-24 10:38:45 -0500

    merging in jacoby work

---
+++ server/db/models/product.js
@@ -0,0 +1,48 @@
+const Category = require('./category')
+const db = require('../db')
+const Sequelize = require('sequelize')
+
+const Product  = db.define('product', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'Product Name',
+        validate: {
+            notEmpty: true
+        }
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        defaultValue: 'Product Description.',
+        validate: {
+            notEmpty: true
+        }
+    },
+    price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+        defaultValue: 0.00,
+        validate: {
+            notEmpty: true
+        }
+    },
+    inventory: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+            notEmpty: true
+        }
+    },
+    imageURL: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'image.jpg',
+        validate: {
+            notEmpty: true
+        }
+    }
+})
+
+module.exports = Product
