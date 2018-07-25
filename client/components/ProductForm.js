3365a4ac04455a30ee4d8fb60076caa1641fc2b1
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-07-24 19:07:04 -0500

    title field works

--- client/components/ProductForm.js
+++ client/components/ProductForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 // TESTER
 
@@ -15,95 +16,126 @@ import React from 'react'
         /> */
 }
 
-const ProductForm = props => (
-  <form onSubmit={props.submitFn}>
-    <div className="input-field">
-      <input
-        type="text"
-        name="title"
-        value={props.product.title}
-        onChange={props.changeFn}
-      />
-      <label className={props.active} htmlFor="title">
-        Product Title
+const titleField = (field) => (
+  <div className="input-field">
+    <input 
+    {...field.input} 
+      type="text"
+      // name="title"
+      // value={props.product.title}
+      // onChange={props.changeFn}
+    />
+    <label htmlFor="title">
+      Product Title
       </label>
-      <span className="helper-text">Cannot be empty</span>
-    </div>
-    <div className="input-field">
-      <input
-        type="number"
-        name="price"
-        value={props.product.price}
-        onChange={props.changeFn}
-      />
-      <label className={props.active} htmlFor="price">
-        Product Price
-      </label>
-      <span className="helper-text">Cannot be empty, must be > 0</span>
-    </div>
-    <div className="input-field">
-      <input
-        type="number"
-        name="inventory"
-        value={props.product.inventory}
-        onChange={props.changeFn}
-      />
-      <label className={props.active} htmlFor="inventory">
-        Product Inventory
-      </label>
-      <span className="helper-text">Cannot be empty, must be >= 0</span>
-    </div>
-    <div className="input-field">
-      <input
-        type="url"
-        name="imageURL"
-        className="validate"
-        value={props.product.imageUrl}
-        onChange={props.changeFn}
-      />
-      <label className={props.active} htmlFor="imageURL">
-        product Image Url
-      </label>
-      <span
-        className="helper-text"
-        data-error="Invalid URL"
-        data-success="Valid URL!"
-      >
-        Must be a valid URL.
-      </span>
-    </div>
-    <div className="input-field">
-      <textarea
-        id="resizeTextArea"
-        name="description"
-        className="materialize-textarea"
-        value={props.product.description}
-        onChange={props.changeFn}
-      />
-      <label className={props.active} htmlFor="description">
-        Product Description
-      </label>
-    </div>
-    <p>Categories Placeholder</p>
-    <button
-      className="btn waves-effect waves-light"
-      disabled={
-        !props.product.title || !props.product.price || !props.product.inventory
-      }
-      type="submit"
-    >
-      Submit
-      <i className="material-icons right">send</i>
-    </button>
-    {/* TODO: Necessary for materialize to init, move to a helper script file
-      and load that in the html file? */}
-    {[
-      () =>
-        setTimeout(() => {
-          M.textareaAutoResize($('#resizeTextArea'))
-        }, 100)
-    ].forEach(func => func())}
-  </form>
+      {field.meta.touched && field.meta.error &&
+    <span className="helper-text">{field.meta.error}</span>}
+  </div>
 )
 
-export default ProductForm
+const ProductForm = props => {
+  const { handleSubmit } = props
+  return (
+  <form onSubmit={handleSubmit}>
+    <div>
+      <Field name="title" component={titleField} />
+    </div>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" type="email" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  // <form onSubmit={props.handleSubmit}>
+  //   <Field name="title" component={titleField}/>
+  //   <div className="input-field">
+  //     <input
+  //       type="number"
+  //       name="price"
+  //       value={props.product.price}
+  //       onChange={props.changeFn}
+  //     />
+  //     <label className={props.active} htmlFor="price">
+  //       Product Price
+  //     </label>
+  //     <span className="helper-text">Cannot be empty, must be > 0</span>
+  //   </div>
+  //   <div className="input-field">
+  //     <input
+  //       type="number"
+  //       name="inventory"
+  //       value={props.product.inventory}
+  //       onChange={props.changeFn}
+  //     />
+  //     <label className={props.active} htmlFor="inventory">
+  //       Product Inventory
+  //     </label>
+  //     <span className="helper-text">Cannot be empty, must be >= 0</span>
+  //   </div>
+  //   <div className="input-field">
+  //     <input
+  //       type="url"
+  //       name="imageURL"
+  //       className="validate"
+  //       value={props.product.imageUrl}
+  //       onChange={props.changeFn}
+  //     />
+  //     <label className={props.active} htmlFor="imageURL">
+  //       product Image Url
+  //     </label>
+  //     <span
+  //       className="helper-text"
+  //       data-error="Invalid URL"
+  //       data-success="Valid URL!"
+  //     >
+  //       Must be a valid URL.
+  //     </span>
+  //   </div>
+  //   <div className="input-field">
+  //     <textarea
+  //       id="resizeTextArea"
+  //       name="description"
+  //       className="materialize-textarea"
+  //       value={props.product.description}
+  //       onChange={props.changeFn}
+  //     />
+  //     <label className={props.active} htmlFor="description">
+  //       Product Description
+  //     </label>
+  //   </div>
+  //   <p>Categories Placeholder</p>
+  //   <button
+  //     className="btn waves-effect waves-light"
+  //     disabled={
+  //       !props.product.title || !props.product.price || !props.product.inventory
+  //     }
+  //     type="submit"
+  //   >
+  //     Submit
+  //     <i className="material-icons right">send</i>
+  //   </button>
+  //   {/* TODO: Necessary for materialize to init, move to a helper script file
+  //     and load that in the html file? */}
+  //   {[
+  //     () =>
+  //       setTimeout(() => {
+  //         M.textareaAutoResize($('#resizeTextArea'))
+  //       }, 100)
+  //   ].forEach(func => func())}
+  // </form>
+  )
+}
+
+
+export default reduxForm({})(ProductForm)
+
+
+
