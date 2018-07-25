2e07353cf8449b6e18ada46e89eaeea419174ae0
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-07-25 18:19:20 -0500

    added reducer add edit capabilities

--- client/store/reviews.js
+++ client/store/reviews.js
@@ -4,6 +4,8 @@ import history from '../history'
 // ACTION TYPES
 
 const GET_REVIEWS = 'GET_REVIEWS'
+const ADD_REVIEWS = 'ADD_REVIEWS'
+const REVIEW_UPDATED = 'REVIEW_UPDATED'
 
 /**
  * INITIAL STATE
@@ -26,19 +28,49 @@ const gotReviews = reviews => ({
   reviews
 })
 
+const addReview = review => ({
+  type: ADD_REVIEWS,
+  review
+})
+
+const reviewUpdated = updatedReview => ({
+  type: REVIEW_UPDATED,
+  updatedReview
+})
+
 export const getReviews = () => dispatch => {
   axios
     .get(`/api/reviews`)
-    .then(({data}) => {
+    .then(({ data }) => {
       dispatch(gotReviews(data))
     }
-  )
+    )
     .catch(error => console.error(error))
+}
+
+export const postReview = (newReview) => dispatch => {
+  axios
+    .post('/api/review', newReview)
+    .then(
+      ({ data }) => {
+        dispatch(addReview(data))
+        history.push(`/product/${data.id}`)
+      }
+    )
+    .catch(error => console.error(error))
+}
+
+export const updateReviewById = review => dispatch => {
+  axios
+    .put(`/api/reviews/${review.id}`, review)
+    .then(({ data }) => dispatch(reviewUpdated(data)))
+    .catch(err => console.error(err))
+
 }
 
 // REDUCER
 
-export default function(state = defaultReviews, action) {
+export default function (state = defaultReviews, action) {
   switch (action.type) {
     case GET_REVIEWS:
       return {
@@ -47,6 +79,20 @@ export default function(state = defaultReviews, action) {
           return result
         }, {}),
         allIds: action.reviews.map(review => review.id)
+      }
+    case ADD_REVIEWS:
+      return {
+        byId: { ...state.byId, [action.review.id]: action.review },
+        allIds: [...state.allIds, action.review.id]
+      }
+
+    case REVIEW_UPDATED:
+      return {
+        byId: {
+          ...state.byId,
+          [action.updatedReview.id]: action.updatedReview
+        },
+        allIds: [...state.allIds]
       }
     default:
       return state
