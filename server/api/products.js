8faf11771f36bbb3ecb32ad18aa3b204b8dbc1e0
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-07-25 16:50:29 -0500

    converting field to json

--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,14 @@ const router = require('express').Router()
 const {Product} = require('../db/models')
 module.exports = router
 
+const createProductFromJSON = body => ({
+  title: '' + body.title,
+  description: '' + body.description,
+  price: +body.price,
+  imageURL: '' + body.imageURL,
+  inventory: +body.inventory
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll({})
