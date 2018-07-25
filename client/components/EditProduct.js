f3d7ab24c90fe173595bac4fa00d4a9148a6bfad
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-07-25 16:48:37 -0500

    edit review server

--- client/components/EditProduct.js
+++ client/components/EditProduct.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import {ProductForm} from '../components'
+import {updateProductById} from '../store'
+import {connect} from 'react-redux'
 
 class EditProducts extends React.Component {
-  submit = values => {
-    console.log(values)
+  submit = editedProduct => {
+    this.props.updateProductById(editedProduct)
+    this.props.history.push(`/product/${this.props.match.params.productId}`)
   }
   render() {
     return <ProductForm {...this.props} onSubmit={this.submit} />
   }
 }
 
-export default EditProducts
+const mapDispatchToProps = dispatch => ({
+  updateProductById: product => dispatch(updateProductById(product))
+})
+
+export default connect(null, mapDispatchToProps)(EditProducts)
+
+
--- server/api/reviews.js
+++ server/api/reviews.js
@@ -10,3 +10,19 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.put('/:reviewId', (req, res, next) => {
+  if (req.body.id && +req.body.id !== +req.params.reviewId) {
+    next(new Error('Bad Request detected in PUT /:reviewId'))
+  } else {
+    Review.update(createReviewFromJSON(req.body), {
+      where: {id: +req.params.reviewId},
+      returning: true
+    })
+      .spread(
+        (done, updatedRev) =>
+          done ? res.json(...updatedRev) : res.status(404).end()
+      )
+      .catch(next)
+  }
+})
