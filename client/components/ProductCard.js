7bc3eadcbbba4167bd930afe459814ff1e0a3148
Patrick Kilgore <pckilgore@noreply.github.com>
2018-07-25 19:11:10 -0500

    Merge in SingleProduct changes

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -1,25 +1,28 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const ProductCard = ({product}) => (
   <div className="col s12 m4 l3">
-    <div className="card large">
-      <div className="card-image">
-        <img src={product.imageURL} />
-      </div>
-      <div className="card-content">
-        <span className="card-title grey-text text-darken-4">
-          {product.title}
-        </span>
-      </div>
-      <div className="card-action">
-        <span>
-          {'$ '}
-          {product.price}
-        </span>
-        <i className="material-icons right green-text" alt="Add to cart">
-          add_shopping_cart
-        </i>
-      </div>
+    <div className="card large hoverable">
+      <Link to={`/product/${product.id}`}>
+        <div className="card-image">
+          <img src={product.imageURL} />
+        </div>
+        <div className="card-content">
+          <span className="card-title grey-text text-darken-4">
+            {product.title}
+          </span>
+        </div>
+        <div className="card-action">
+          <span>
+            {'$ '}
+            {product.price}
+          </span>
+          <i className="material-icons right green-text" alt="Add to cart">
+            add_shopping_cart
+          </i>
+        </div>
+      </Link>
     </div>
   </div>
 )
--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,40 +1,62 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {AddToCartButton} from '../components'
 
-class SingleProduct extends React.Component {
-  render() {
-    const {title, imageURL, description, inventory, price} = this.props.product
-    return (
-      <div className="container">
-        <div className="row">
+const SingleProduct = props => {
+  const {title, imageURL, description, inventory, price} = props.product
+  return (
+    <div className="container">
+      <br />
+      <div className="row">
+        <div className="col s6">
+          <h4>{title}</h4>
+          <div className="col s6">${price}</div>
           <div className="col s6">
-            <h4>{title}</h4>
-            <h6>${price}</h6>
-            <h6>Qty: {inventory}</h6>
-            <p>{description}</p>
-            <div className="row">
-              <Link
-                to={`${this.props.match.url}/edit`}
-                className="col s3 waves-effect waves-light btn"
-              >
-                Edit Product
-              </Link>
-              <a className="col s3 waves-effect waves-light btn">Add to Cart</a>
-            </div>
+            <AddToCartButton prodId={props.product.id} />
           </div>
-          <div className="col s6">
-            <img src={imageURL} />
-          </div>
-          <div className="col s12">
-            <h5>Reviews</h5>
-            <h6>First Review Title</h6>
-            <p>Reviews content here...</p>
+          <h4>{title}</h4>
+          <h6>${price}</h6>
+          <h6>Qty: {inventory}</h6>
+          <p>{description}</p>
+          <div className="row">
+            <Link
+              to={`${props.match.url}/edit`}
+              className="col s3 waves-effect waves-light btn"
+            >
+              Edit Product
+            </Link>
+            <a className="col s3 waves-effect waves-light btn">Add to Cart</a>
           </div>
         </div>
+        <div className="col s6">
+          <img src={imageURL} />
+        </div>
+        <div className="col s12">
+          <h5>Reviews</h5>
+          <h6>First Review Title</h6>
+          <p>Reviews content here...</p>
+        </div>
+        <h6>Product Stock: {inventory}</h6>
+        <p>{description}</p>
+        <Link
+          to={`${props.match.url}/edit`}
+          className="waves-effect red waves-light btn"
+        >
+          Edit Product
+          <i className="material-icons right">edit</i>
+        </Link>
+        <div className="col s6">
+          <img src={imageURL} />
+        </div>
+        <div className="col s12">
+          <h5>Reviews</h5>
+          <h6>First Review Title</h6>
+          <p>Reviews content here...</p>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = (state, ownProps) => {
