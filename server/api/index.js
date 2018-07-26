259195eec689928d107cc25f766a38a8dfe2a866
Patrick Kilgore <pckilgore@noreply.github.com>
2018-07-26 18:02:15 -0500

    move over stashed chaanges from wrong branch

--- server/api/index.js
+++ server/api/index.js
@@ -3,7 +3,9 @@ module.exports = router
 
 router.use('/users', require('./users'))
 router.use('/products', require('./products'))
-router.use('/carts', require('./carts'))
+router.use('/categories', require('./categories'))
+router.use('/productcategories', require('./productCategories'))
+router.use('/carts/', require('./carts'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
