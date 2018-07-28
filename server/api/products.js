6911980900a821c2c1559af935ed0fbd12171012
Michael Bush <38108504+michaelegregious@users.noreply.github.com>
2018-07-27 19:02:01 -0500

    Added authMiddleware, moved add/edit product routes to admin-protected routes (#98)
    * changes after rebasing
    
    * moved add and edit products routes to admin and updated
    
    * admin blocking on backend implemented but products tests disabled

--- server/api/products.js
+++ server/api/products.js
@@ -1,15 +1,7 @@
 const router = require('express').Router()
 const {Product} = require('../db/models')
-module.exports = router
 
-// Helper functions to shape database requests.
-const createProductFromJSON = body => ({
-  title: '' + body.title,
-  description: '' + body.description,
-  price: +body.price,
-  imageURL: '' + body.imageURL,
-  inventory: +body.inventory
-})
+module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
@@ -17,39 +9,5 @@ router.get('/', async (req, res, next) => {
     res.json(products)
   } catch (err) {
     next(err)
-  }
-})
-
-router.post('/', async (req, res, next) => {
-  try {
-    const products = await Product.create({
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price,
-      inventory: req.body.inventory,
-      imageURL: req.body.imageURL
-    })
-    res.json(products)
-  } catch (err) {
-    err.status = 400
-    err.message = {error: err.message}
-    next(err)
-  }
-})
-
-/* TODO: Add isAdmin*/
-router.put('/:productId', (req, res, next) => {
-  if (req.body.id && +req.body.id !== +req.params.productId) {
-    next(new Error('Bad Request detected in PUT /:productId'))
-  } else {
-    Product.update(createProductFromJSON(req.body), {
-      where: {id: +req.params.productId},
-      returning: true
-    })
-      .spread(
-        (done, updatedProd) =>
-          done ? res.json(...updatedProd) : res.status(404).end()
-      )
-      .catch(next)
   }
 })
