cd2b32f8f7b8ae22e659ee7bfba34d69035cd8f0
Jacoby Kang <jacob.kang@vanderbilt.edu>
2018-07-31 13:33:55 -0500

    product with no quantity will not have a add cart option

--- client/components/AddToCartButton.js
+++ client/components/AddToCartButton.js
@@ -6,20 +6,21 @@ class AddToCartButton extends React.Component {
   // Expects a prodId prop to be give to it from higher order component.
   // Also expects a dumb button that can take an 'add' prop to be given to it.
   add = () => {
-    const {productId, quantity} = this.props
+    const {productId, modifyCartQuantity} = this.props
     if (this.props.isInCart) {
-      this.props.editItemInCart({productId, quantity})
-    } else this.props.addItemToCart({productId, quantity})
+      this.props.editItemInCart({productId, modifyCartQuantity})
+    } else this.props.addItemToCart({productId, modifyCartQuantity})
   }
 
   render() {
     const WrappedButton = this.props.buttonTypeComponent
-    return <WrappedButton add={this.add} />
+    return <WrappedButton add={this.add} productQuantity={this.props.productQuantity}/>
   }
 }
 const mapStateToProps = (state, {productId}) => ({
-  quantity: addToCartQuantity(state.cart, productId),
-  isInCart: !!state.cart.byProductId[productId]
+  modifyCartQuantity: addToCartQuantity(state.cart, productId),
+  isInCart: !!state.cart.byProductId[productId],
+  productQuantity: state.products.byId[productId].inventory
 })
 
 const mapDispatchToProps = dispatch => ({
