0dd6bb78c1f4e845436a11f67c60500e47c8fc8e
Patrick Kilgore <pckilgore@noreply.github.com>
2018-07-31 21:15:01 -0500

    Can now add to cart from allProducts Page

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import ModifyCartButton from './ModifyCartButton'
 
 const ProductCard = ({product}) => (
   <div className="col s12 m6 xl4">
@@ -14,16 +15,31 @@ const ProductCard = ({product}) => (
             {product.title}
           </span>
         </div>
-        <div className="card-action">
-          <span>
-            {'$ '}
-            {product.price}
-          </span>
-          <i className="material-icons right green-text" alt="Add to cart">
-            add_shopping_cart
-          </i>
-        </div>
       </Link>
+      <div className="card-action">
+        <span>
+          {'$ '}
+          {product.price}
+        </span>
+        <ModifyCartButton
+          productId={product.id}
+          actionName="add"
+          buttonTypeComponent={props => (
+            <button
+              className="btn-floating white right"
+              alt="Add to Cart"
+              type="button"
+              onClick={props.modifyCart}
+              disabled={props.disabled}
+            >
+              <i className="material-icons green-text" alt="Add to cart">
+                add_shopping_cart
+              </i>{' '}
+            </button>
+          )}
+          nextQuantity={quantity => ++quantity}
+        />
+      </div>
     </div>
   </div>
 )
